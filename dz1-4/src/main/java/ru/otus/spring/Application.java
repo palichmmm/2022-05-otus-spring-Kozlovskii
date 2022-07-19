217ce8466d1d244fc676f6759de5d01478f64df3
Евгений Козловский <palichmmm@gmail.com>
2022-07-19 09:54:49 +0700

    Внесение поправок

--- dz1-4/src/main/java/ru/otus/spring/Application.java
+++ dz1-4/src/main/java/ru/otus/spring/Application.java
@@ -2,21 +2,14 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import ru.otus.spring.service.*;
+import ru.otus.spring.service.TestLaunch;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		var context = SpringApplication.run(Application.class, args);
-		QuestionService questionService = context.getBean(QuestionServiceImpl.class);
-		QuestionConvert questionConvert = context.getBean(QuestionConvertImpl.class);
-		MixAnswersQuestionsService mixAnswersQuestionsService = context.getBean(MixAnswersQuestionsServiceImpl.class);
-		LocaleService localeService = context.getBean(LocaleServiceImpl.class);
-		IOService ioService = new IOServiceStreamsImpl(System.out, System.in);
-		new TestLaunch(ioService, questionConvert, questionService, mixAnswersQuestionsService, localeService).run();
-		context.close();
+		context.getBean(TestLaunch.class).run();
 	}
 
 }

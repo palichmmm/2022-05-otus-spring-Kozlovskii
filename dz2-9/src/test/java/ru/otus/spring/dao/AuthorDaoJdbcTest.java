778adf049f564f8f6f521373256a35ddaa9305b0
Евгений Козловский <palichmmm@gmail.com>
2022-08-13 20:28:09 +0700

    Удаление лишнего импорта

--- dz2-9/src/test/java/ru/otus/spring/dao/AuthorDaoJdbcTest.java
+++ dz2-9/src/test/java/ru/otus/spring/dao/AuthorDaoJdbcTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.dao.EmptyResultDataAccessException;
 import ru.otus.spring.models.Author;
 
 import java.util.List;

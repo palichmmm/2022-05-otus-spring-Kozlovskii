1fada2702e8280167d44bc7a7934b90c2531a30e
Евгений Козловский <palichmmm@gmail.com>
2022-11-24 11:18:10 +0700

    Валидация книги

--- dz3-18/src/main/java/ru/otus/spring/controller/BookController.java
+++ dz3-18/src/main/java/ru/otus/spring/controller/BookController.java
@@ -2,7 +2,10 @@ package ru.otus.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.otus.spring.dto.BookDTO;
 import ru.otus.spring.models.Author;
 import ru.otus.spring.models.Book;
 import ru.otus.spring.models.Genre;
@@ -10,6 +13,7 @@ import ru.otus.spring.repositories.AuthorRepository;
 import ru.otus.spring.repositories.BookRepository;
 import ru.otus.spring.repositories.GenreRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -33,33 +37,41 @@ public class BookController {
     }
 
     @GetMapping("/book/edit/{id}")
-    public String one(@PathVariable("id") long id, Model model) {
-        Book book = repository.findById(id).orElseThrow(RuntimeException::new);
+    public String edit(@ModelAttribute("book") BookDTO book, Model model) {
+        Book editBook = repository.findById(book.getId()).orElseThrow(RuntimeException::new);
         List<Author> authors = authorRepository.findAll();
         List<Genre> genres = genreRepository.findAll();
         model.addAttribute("authors", authors);
         model.addAttribute("genres", genres);
-        model.addAttribute("book", book);
+        book.setBookName(editBook.getBookName());
+        book.setAuthor(editBook.getAuthor().getAuthorName());
+        book.setGenre(editBook.getGenre().getGenreName());
         return "/book/edit";
     }
 
+    @Validated
     @PostMapping("/book/edit/{id}")
-    public String edit(@PathVariable("id") long id,
-                       @RequestParam("name") String name,
-                       @RequestParam("author") String author,
-                       @RequestParam("genre") String genre) {
-        Book book = repository.findById(id).orElseThrow(RuntimeException::new);
-        Author newAuthor = authorRepository.findByAuthorName(author).orElseThrow(RuntimeException::new);
-        Genre newGenre = genreRepository.findByGenreName(genre).orElseThrow(RuntimeException::new);
-        book.setBookName(name);
-        book.setAuthor(newAuthor);
-        book.setGenre(newGenre);
-        repository.save(book);
+    public String edit(@Valid @ModelAttribute("book") BookDTO book,
+                       BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            List<Author> authors = authorRepository.findAll();
+            List<Genre> genres = genreRepository.findAll();
+            model.addAttribute("authors", authors);
+            model.addAttribute("genres", genres);
+            return "/book/edit";
+        }
+        Book editBook = repository.findById(book.getId()).orElseThrow(RuntimeException::new);
+        Author newAuthor = authorRepository.findByAuthorName(book.getAuthor()).orElseThrow(RuntimeException::new);
+        Genre newGenre = genreRepository.findByGenreName(book.getGenre()).orElseThrow(RuntimeException::new);
+        editBook.setBookName(book.getBookName());
+        editBook.setAuthor(newAuthor);
+        editBook.setGenre(newGenre);
+        repository.save(editBook);
         return "redirect:/book/all";
     }
 
     @GetMapping("/book/create")
-    public String create(Model model) {
+    public String create(@ModelAttribute("book") BookDTO book, Model model) {
         List<Author> authors = authorRepository.findAll();
         List<Genre> genres = genreRepository.findAll();
         model.addAttribute("authors", authors);
@@ -67,14 +79,21 @@ public class BookController {
         return "/book/create";
     }
 
+    @Validated
     @PostMapping("/book/create")
-    public String edit(@RequestParam("name") String name,
-                       @RequestParam("author") String author,
-                       @RequestParam("genre") String genre) {
-        Author newAuthor = authorRepository.findByAuthorName(author).orElseThrow(RuntimeException::new);
-        Genre newGenre = genreRepository.findByGenreName(genre).orElseThrow(RuntimeException::new);
-        Book book = new Book(name, newAuthor, newGenre);
-        repository.save(book);
+    public String create(@Valid @ModelAttribute("book") BookDTO book,
+                         BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            List<Author> authors = authorRepository.findAll();
+            List<Genre> genres = genreRepository.findAll();
+            model.addAttribute("authors", authors);
+            model.addAttribute("genres", genres);
+            return "/book/create";
+        }
+        Author newAuthor = authorRepository.findByAuthorName(book.getAuthor()).orElseThrow(RuntimeException::new);
+        Genre newGenre = genreRepository.findByGenreName(book.getGenre()).orElseThrow(RuntimeException::new);
+        Book newBook = new Book(book.getBookName(), newAuthor, newGenre);
+        repository.save(newBook);
         return "redirect:/book/all";
     }
 
@@ -86,7 +105,7 @@ public class BookController {
     }
 
     @DeleteMapping("/book/delete/{id}")
-    public @ResponseBody String edit(@PathVariable("id") long id) {
+    public @ResponseBody String delete(@PathVariable("id") long id) {
         repository.deleteById(id);
         return "/book/all";
     }

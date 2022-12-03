a47a07c9f6cbaefb79507552517a11b18afef429
Евгений Козловский <palichmmm@gmail.com>
2022-12-03 19:32:32 +0700

    Удаление ненужных импортов

--- dz3-18/src/main/java/ru/otus/spring/controller/CommentController.java
+++ dz3-18/src/main/java/ru/otus/spring/controller/CommentController.java
@@ -3,7 +3,6 @@ package ru.otus.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.spring.dto.CommentDTO;
 import ru.otus.spring.models.Book;

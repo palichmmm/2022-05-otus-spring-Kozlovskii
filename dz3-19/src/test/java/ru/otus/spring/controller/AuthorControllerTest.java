2f7cc7e2905fdc7cc448c6f2f05b7931b751d417
Евгений Козловский <palichmmm@gmail.com>
2022-12-12 20:42:18 +0700

    Тесты rest controller авторов и жанров

--- dz3-19/src/test/java/ru/otus/spring/controller/AuthorControllerTest.java
+++ dz3-19/src/test/java/ru/otus/spring/controller/AuthorControllerTest.java
@@ -1,11 +1,13 @@
 package ru.otus.spring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.spring.models.Author;
 import ru.otus.spring.rest.AuthorController;
@@ -14,8 +16,10 @@ import ru.otus.spring.service.AuthorService;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthorController.class)
 class AuthorControllerTest {
@@ -24,44 +28,50 @@ class AuthorControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private AuthorService authorService;
 
-    @DisplayName("Страница всех авторов должна вернуть статус 200")
+    @DisplayName("REST API всех авторов должна вернуть статус 200 и JSON")
     @Test
-    void AllAuthorsPageShouldReturnStatus200() throws Exception {
+    void AuthorsREST_API_ShouldReturnStatus200AndJSON() throws Exception {
         List<Author> authors = List.of(new Author(1, "Автор1"), new Author(2, "Автор2"));
+        String expectedResult = mapper.writeValueAsString(authors);
         Mockito.when(authorService.findAll()).thenReturn(authors);
-        mvc.perform(get("/author/all"))
+        mvc.perform(get("/api/author"))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/author/all"))
-                .andExpect(model().attribute("authors", authors))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResult))
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("Страница редактирования автора должна вернуть статус 200")
+    @DisplayName("REST API автора должна вернуть статус 200 и JSON")
     @Test
-    void AuthorEditFormPageShouldReturnStatus200() throws Exception {
+    void AuthorREST_API_ShouldReturnStatus200AndJSON() throws Exception {
         Author author = new Author(AUTHOR_ID, "Автор");
+        String expectedResult = mapper.writeValueAsString(author);
         Mockito.when(authorService.findById(AUTHOR_ID)).thenReturn(author);
-        mvc.perform(get("/author/edit/{id}", AUTHOR_ID))
+        mvc.perform(get("/api/author/{id}", AUTHOR_ID))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/author/edit"))
-                .andExpect(model().attribute("author", author))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResult))
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("Страница создания автора должна вернуть статус 200")
+    @DisplayName("REST API создания автора должна вернуть статус 200 и JSON")
     @Test
-    void AuthorCreationFormPageShouldReturnStatus200() throws Exception {
-        Author author = new Author();
-        mvc.perform(get("/author/create"))
+    void AuthorCreation_REST_API_ShouldReturnStatus200AndJSON() throws Exception {
+        Author author = new Author(0, "Автор");
+        String postBody = mapper.writeValueAsString(author);
+        Mockito.when(authorService.save(author)).thenReturn(author);
+        mvc.perform(post("/api/author")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postBody))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/author/create"))
-                .andExpect(model().attribute("author", author))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(postBody))
                 .andExpect(status().isOk());
     }
 }
--- dz3-19/src/test/java/ru/otus/spring/controller/GenreControllerTest.java
+++ dz3-19/src/test/java/ru/otus/spring/controller/GenreControllerTest.java
@@ -1,11 +1,13 @@
 package ru.otus.spring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.spring.models.Genre;
 import ru.otus.spring.rest.GenreController;
@@ -14,6 +16,7 @@ import ru.otus.spring.service.GenreService;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,44 +27,48 @@ public class GenreControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private GenreService genreService;
 
-    @DisplayName("Страница всех жанров должна вернуть статус 200")
+    @DisplayName("REST API всех жанров должна вернуть статус 200 и JSON")
     @Test
-    void AllGenresPageShouldReturnStatus200() throws Exception {
+    void GenresREST_API_ShouldReturnStatus200AndJSON() throws Exception {
         List<Genre> genres = List.of(new Genre(GENRE_ID, "Жанр1"), new Genre(2, "Жанр2"));
+        String expectedResult = mapper.writeValueAsString(genres);
         Mockito.when(genreService.findAll()).thenReturn(genres);
-        mvc.perform(get("/genre/all"))
+        mvc.perform(get("/api/genre"))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/genre/all"))
-                .andExpect(model().attribute("genres", genres))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResult))
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("Страница редактирования жанра должна вернуть статус 200")
+    @DisplayName("REST API автора должна вернуть статус 200 и JSON")
     @Test
-    void GenreEditFormPageShouldReturnStatus200() throws Exception {
+    void GenreREST_API_ShouldReturnStatus200AndJSON() throws Exception {
         Genre genre = new Genre(GENRE_ID, "Жанр");
+        String expectedResult = mapper.writeValueAsString(genre);
         Mockito.when(genreService.findById(GENRE_ID)).thenReturn(genre);
-        mvc.perform(get("/genre/edit/{id}", GENRE_ID))
+        mvc.perform(get("/api/genre/{id}", GENRE_ID))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/genre/edit"))
-                .andExpect(model().attribute("genre", genre))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResult))
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("Страница создания жанра должна вернуть статус 200")
+    @DisplayName("REST API создания жанра должна вернуть статус 200 и JSON")
     @Test
-    void GenreCreateFormPageShouldReturnStatus200() throws Exception {
-        Genre genre = new Genre();
-        mvc.perform(get("/genre/create"))
+    void GenreCreation_REST_API_ShouldReturnStatus200AndJSON() throws Exception {
+        Genre genre = new Genre(0, "Жанр");
+        String postBody = mapper.writeValueAsString(genre);
+        Mockito.when(genreService.save(genre)).thenReturn(genre);
+        mvc.perform(post("/api/genre").contentType(MediaType.APPLICATION_JSON).content(postBody))
                 .andDo(print())
-                .andExpect(content().contentType("text/html;charset=UTF-8"))
-                .andExpect(view().name("/genre/create"))
-                .andExpect(model().attribute("genre", genre))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(postBody))
                 .andExpect(status().isOk());
     }
 }

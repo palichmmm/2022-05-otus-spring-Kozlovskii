1bfb23f957222e9e41e028d85f8748a9461bebd2
Евгений Козловский <palichmmm@gmail.com>
2023-01-05 20:37:21 +0700

    Внесение рекомендуемых правок

--- dz3-22/src/main/java/ru/otus/spring/controller/rest/BookRestController.java
+++ dz3-22/src/main/java/ru/otus/spring/controller/rest/BookRestController.java
@@ -6,9 +6,13 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.spring.dto.BookDto;
 import ru.otus.spring.dto.BookDtoMapper;
+import ru.otus.spring.models.Author;
 import ru.otus.spring.models.Book;
+import ru.otus.spring.models.Genre;
+import ru.otus.spring.repository.AuthorRepository;
 import ru.otus.spring.repository.BookRepository;
 import ru.otus.spring.repository.CommentRepository;
+import ru.otus.spring.repository.GenreRepository;
 
 @RestController
 public class BookRestController {
@@ -17,12 +21,18 @@ public class BookRestController {
 
     private final BookDtoMapper bookDtoMapper;
     private final CommentRepository commentRepository;
+    private final AuthorRepository authorRepository;
+    private final GenreRepository genreRepository;
 
     public BookRestController(BookRepository repository, BookDtoMapper bookDtoMapper,
-                              CommentRepository commentRepository) {
+                              CommentRepository commentRepository,
+                              AuthorRepository authorRepository,
+                              GenreRepository genreRepository) {
         this.repository = repository;
         this.bookDtoMapper = bookDtoMapper;
         this.commentRepository = commentRepository;
+        this.authorRepository = authorRepository;
+        this.genreRepository = genreRepository;
     }
 
     @GetMapping("/api/book")
@@ -38,16 +48,24 @@ public class BookRestController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/api/book")
     public Mono<BookDto> saveBook(@RequestBody BookDto bookDto) {
-        Book realBook = bookDtoMapper.toBook(bookDto, new Book(null));
-        return repository.save(realBook).map(bookDtoMapper::toDto);
+        Mono<Author> authorMono = authorRepository.findById(bookDto.getAuthorId());
+        Mono<Genre> genreMono = genreRepository.findById(bookDto.getGenreId());
+        return Mono.zip(authorMono, genreMono, (author, genre) ->
+                        new Book(null, bookDto.getBookName(), author, genre)
+                )
+                .flatMap(repository::save)
+                .map(bookDtoMapper::toDto);
     }
 
     @PutMapping(value = "/api/book")
     public Mono<BookDto> updateBook(@RequestBody BookDto bookDto) {
-        return repository.findById(bookDto.getId()).flatMap(book -> {
-            Book realBook = bookDtoMapper.toBook(bookDto, book);
-            return repository.save(realBook);
-        }).map(bookDtoMapper::toDto);
+        Mono<Author> authorMono = authorRepository.findById(bookDto.getAuthorId());
+        Mono<Genre> genreMono = genreRepository.findById(bookDto.getGenreId());
+        return Mono.zip(authorMono, genreMono, (author, genre) ->
+                        new Book(bookDto.getId(), bookDto.getBookName(), author, genre)
+                )
+                .flatMap(repository::save)
+                .map(bookDtoMapper::toDto);
     }
 
     @DeleteMapping("/api/book/{id}")

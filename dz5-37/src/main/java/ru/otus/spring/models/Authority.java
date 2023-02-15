bc79924a379880d3c9f7726292eddb200f88acfd
Евгений Козловский <palichmmm@gmail.com>
2023-02-15 12:09:39 +0700

    Копирование проекта

---
+++ dz5-37/src/main/java/ru/otus/spring/models/Authority.java
@@ -0,0 +1,26 @@
+package ru.otus.spring.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    private long id;
+    @Column(name = "username")
+    private String userName;
+    @Column(name="authority")
+    private String authority;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="username", insertable = false,updatable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private User user;
+}
